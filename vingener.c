0875ecaf00b0bf61b67d44f4ae3225a5dcd398a4
Orest Klymko <Orest Klymko>
2016-10-29 22:03:00 +0300

    final version

--- vingener.c
+++ vingener.c
@@ -7,45 +7,46 @@
 
 int main(int argc, string argv[])
 {
-string key = argv[1];
-if (argc !=2)
-{
-printf("Try again");
-return 1;
-}
-for (int i = 0; i < strlen(key); i++)
-{
-if (!isalpha(key[i]))
-{
-printf("Try again");
-return 1;
-}
-}
-string text;
-text = GetString();
-char keyLengthOfText[strlen(text)];
+	string key = argv[1];
+	if (argc != 2)
+	{
+		printf("Try again");
+		return 1;
+	}
+	for (int i = 0; i < strlen(key); i++)
+	{
+		if (!isalpha(key[i]))
+		{
+			printf("Try again");
+			return 1;
+		}
+	}
+	string txt
+		txt = GetString();;/*присвоєння змінній txt значення GetString */
+
+	char keytxt[strlen(txt)];/*присвоєння змінній keytxt типу char */
 
 
-for ( int i = 0, j = 0; i < strlen(text); i++)
-{
-if (((text[i] >= 65) && (text[i] <= 90))
-|| ((text[i] >= 97) && (text[i] <= 122)))
-{
-keyLengthOfText[i] = keyword[j % strlen(keyword)];
-j++;
-}
-}
-for (int i = 0; i < strlen(text); i++)
-{
-if ((text[i] >= 65) && (text[i] <= 90))
-{
-text[i] = 65 + (text[i] - 65 + toupper(keyLengthOfText[i]) - 65)%26;
-}
-else if ((text[i] >= 97) && (text[i] <= 122))
-{
-text[i] = 97 + (text[i] - 97 + tolower(keyLengthOfText[i]) - 97)%26;
-}
-}
-printf("%s\n", text);
-return 0;
+	for (int i = 0, j = 0; i < strlen(txt); i++)
+	{
+		if (((txt[i] >= 65) && (txt[i] <= 90))
+			|| ((txt[i] >= 97) && (txt[i] <= 122)))
+		{
+			keytxt[i] = generalword[j % strlen(generalword)];
+			j++;
+		}
+	}
+	for (int i = 0; i < strlen(txt); i++)
+	{
+		if ((txt[i] >= 65) && (txt[i] <= 90))
+		{
+			txt[i] = 65 + (txt[i] - 65 + toupper(keytxt[i]) - 65) % 26;
+		}
+		else if ((txt[i] >= 97) && (txt[i] <= 122))
+		{
+			txt[i] = 97 + (txt[i] - 97 + tolower(keytxt[i]) - 97) % 26;
+		}
+	}
+	printf("%s\n", txt); /* вивід на дисплей значення змінної txt */
+	return 0;
 }
